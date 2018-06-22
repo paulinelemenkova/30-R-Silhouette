4d05f823538ee8a12b0ee7320f3a5f60162c04bb
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2018-06-22 15:54:42 +0800

    Add files via upload

---
+++ Silhouette.R
@@ -0,0 +1,17 @@
+# шаг-1. вчитываем таблицу. делаем из нее датафрейм.
+MarianaTrench_DF <- read.csv("Morphology.csv", header=TRUE, sep = ",")
+MarianaTrench_DF <- na.omit(MarianaTrench_DF) 
+row.has.na <- apply(MarianaTrench_DF, 1, function(x){any(is.na(x))}) # проверяем, удалил ли все NA
+sum(row.has.na) # суммируем все NA, должно получиться: [1] 0
+head(MarianaTrench_DF)
+
+# шаг-2. делаем график "силуэт" библиотекой cluster
+library(cluster)
+pr5 <- pam(MarianaTrench_DF, 6)
+str(si <- silhouette(pr5))
+#plot(si, col = c("lavender", "lightgoldenrod1", "darkseagreen1", "orchid1", "skyblue1", "lightslateblue"))
+#plot(si, col = c("aquamarine", "aquamarine1", "aquamarine2", "aquamarine3", "aquamarine4", "aquamarine4"))
+plot(si, col = c("thistle", "thistle1", "thistle2", "thistle3", "thistle4", "slategray4"))
+
+
+
